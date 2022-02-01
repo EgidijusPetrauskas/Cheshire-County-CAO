bf00a4e48cce2c26d312727fa68eff7ff63b8823
E.Petrauskas <egidijus04petrauskas@gmail.com>
2022-02-01 18:07:31 +0200

    last sections

--- swiper.js
+++ swiper.js
@@ -4,4 +4,9 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
 });
