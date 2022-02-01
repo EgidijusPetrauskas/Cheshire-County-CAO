6587b8dd328f5104f1107e896bc1190bf6add1af
E.Petrauskas <egidijus04petrauskas@gmail.com>
2022-02-01 19:37:10 +0200

    cleaning

--- swiper.js
+++ swiper.js
@@ -1,9 +1,16 @@
 "use strict";
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper1', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
+});
+
+const swiper2 = new Swiper('.swiper2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
 
     // If we need pagination
     pagination: {
